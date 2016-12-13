49e49b0f10e7da07d8ad52d34c3048e4f0e71c0e
sandesh <sandesh@nukart.in>
2016-12-13 12:37:26 +0530

    changes6

--- nuSecond/js/custom.js
+++ nuSecond/js/custom.js
@@ -318,17 +318,6 @@ jQuery(function($){
 
 
 	/* ----------------------------------------------------------- */
-	/*  13. PRELOADER 
-	/* ----------------------------------------------------------- */ 
-
-	  jQuery(window).load(function() { // makes sure the whole site is loaded
-      $('#status').fadeOut(); // will first fade out the loading animation
-      $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-      $('body').delay(100).css({'overflow':'visible'});
-    })
-
-
-	/* ----------------------------------------------------------- */
 	/*  14. MENU SCROLL 
 	/* ----------------------------------------------------------- */
 
